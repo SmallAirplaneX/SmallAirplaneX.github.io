beec09ac804bd3511a3dbee2feb813b6fc15b340
SmallAirplaneX <2321764325@qq.com>
2025-09-21 17:09:31 +0800

    删除多余文件

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import { basicRoutes } from "@/router/routers";
 
 const router = createRouter({
-  history: createWebHashHistory("/"),
+  history: createWebHistory(),
   routes: basicRoutes,
 });
 
